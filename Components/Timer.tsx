eb8d9a4368c1a39bc28095222125e98548a45a19
1503014 <mundhegitanjali@gmail.com>
2019-11-27 12:46:00 +0530

    1 file modified

--- Components/Timer.tsx
+++ Components/Timer.tsx
@@ -1,5 +1,34 @@
 import React, {PureComponent} from 'react';
-import Render from 'react-dom';
+import {render} from 'react-dom';
 
-
-
+export default class Timer extends PureComponent{
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentDate : new Date().toTimeString()
+    };
+}
+private refreshState()
+{
+  this.setState(
+    {
+      currentDate : new Date().toTimeString()
+    }
+  );
+}
+componentDidMount()
+{
+  this.functionHandler = setInterval(() => this.refreshState(), 1000);
+}
+componentWillUnmount()
+{
+  clearInterval(this.functionHandler);
+}
+  render() {
+    return (
+      <div>
+        <div>{this.state.currentDate}</div> 
+      </div>
+    );
+  }
+}
