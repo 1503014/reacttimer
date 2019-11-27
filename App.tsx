243a737f8fde78da1af4ff59e7c5d248e4eb77bf
1503014 <mundhegitanjali@gmail.com>
2019-11-27 12:10:53 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Timer from './Components/Timer'
+function Click(a)
+{
+  return (
+    alert(a)
+  )
+}
+export default function App() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# reacttimer
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/reacttimer)
